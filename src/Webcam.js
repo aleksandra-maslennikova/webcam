b2a1e8da1da12261f0e362edec10539134ad0c84
aleksandra-maslennikova <alex.maslennikova19@gmail.com>
2021-12-23 17:37:55 +0200

    Add rear camera

--- src/Webcam.js
+++ src/Webcam.js
@@ -1,6 +1,5 @@
 import React, { useRef, useCallback, useState } from "react";
 import Webcam from "react-webcam";
-
 const videoConstraints = {
   width: 500,
   height: 500,
@@ -10,6 +9,7 @@ const videoConstraints = {
 export const WebcamCapture = () => {
   const webcamRef = useRef(null);
   const [image, setImage] = useState("");
+  const [facingMode, setFacingMode] = useState("user");
 
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
@@ -17,18 +17,20 @@ export const WebcamCapture = () => {
   }, [webcamRef]);
 
   return (
-    <>
+    <h3>
+      <h3> Mode:{facingMode === "user" ? "Front" : "Rear"}</h3>
       <Webcam
         audio={false}
         height={500}
         ref={webcamRef}
         screenshotFormat="image/jpeg"
         width={500}
-        videoConstraints={videoConstraints}
+        videoConstraints={{ ...videoConstraints, facingMode }}
       />
-
       <button onClick={capture}>Capture photo</button>
+      <button onClick={() => setFacingMode("user")}>Front camera</button>
+      <button onClick={() => setFacingMode("environment")}>Rear camera</button>
       {image && <img src={image}></img>}
-    </>
+    </h3>
   );
 };
